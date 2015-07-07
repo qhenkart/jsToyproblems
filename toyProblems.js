60d7e84b2e0e480ceadca16aa1444709b3128fb1
qhenkart <qhenkart@gmail.com>
2015-07-06 17:11:38 -0700

    rotateMatrix in place

--- toyProblems.js
+++ toyProblems.js
@@ -1,3 +1,4 @@
+//check to see if a string is filled with unique characters without using any Data structures other than Arrays
 function unique(str){
   var map = [];
   for(var i = 0; i <=65; i++){
@@ -15,6 +16,7 @@ function unique(str){
   return true;
 }
 
+//check to see if two strings are permutations of each other
 function isPermutation(str1, str2){
   if(str1.split('').sort().join('') === str2.split('').sort().join('')){
     return true;
@@ -23,6 +25,7 @@ function isPermutation(str1, str2){
   }
 }
 
+//reverse a string without using the reverse method
 function reverse(str){
   str = str.split('')
   for(var i = 0; i < Math.floor(str.length/2); i++){
@@ -33,11 +36,10 @@ function reverse(str){
   return str.join('');
 }
 
-
+//turn a string such as aaaabbbbbccc into a4b5c3
 function freq(str){
-  var newString, previous, count;
-  newString = '';
-  previous = str[0];
+  var newString = '',
+  previous = str[0],
   count = 0;
   for(var i = 0; i < str.length; i++){
     if(previous === str[i]){
@@ -52,6 +54,45 @@ function freq(str){
   return newString.length > str.length ? str : newString;
 }
 
-function test4()
+function rotateMatrix(matrix){
+  var rotated = [];
+  for(var i = 0; i < matrix.length; i++){
+    rotated.push([]);
+  }
 
-console.log(test3('ttttteeeeasdfee'))
+  for(var i = 0; i < matrix.length; i++){
+    for(var j = 0; j < matrix.length; j++){
+      rotated[j][matrix.length-1-i] = matrix[i][j] 
+    }
+  }
+  return rotated;
+}
+
+//rotate a matrix in place by 90 degrees
+function rotateMatrixInPlace(matrix){
+  for(var layer = 0; layer < matrix.length / 2; layer++){
+    var first = layer;
+    var last = matrix.length-1-layer;
+    for(var i = first; i < last; i++){
+      var offset = i - layer;
+      var top = matrix[first][i];
+
+      //left to top
+      matrix[first][i] = matrix[last - offset][first];
+
+      //bottom to left
+      matrix[last-offset][first] = matrix[last][last - offset];
+
+      // right to bottom
+      matrix[last][last-offset] = matrix[i][last];
+
+      //top to right
+      matrix[i][last] = top;
+    }
+  }
+  return matrix
+}
+
+// var square = [[0,1,2],[3,4,5],[6,7,8]] //630 741 852
+// square = [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]] // 12 8 4 0   13 9 5 1   14 10 6 2   15 11 7 3
+// square = [[0,1,2,3,4],[5,6,7,8,9],[10,11,12,13,14],[15,16,17,18,19],[20,21,22,23,24]]; // 20 15 10 5 0     21 16 11 6 1    22 17 12 7 2   23 18 13 8 3   24 19 14 9 4 
